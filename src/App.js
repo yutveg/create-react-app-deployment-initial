1b7afde7250746445de0467636e9a6fcefaeacc2
jarrodskahill <jarrodskahill@gmail.com>
2019-12-19 10:47:25 -0800

    greeting

--- src/App.js
+++ src/App.js
@@ -26,6 +26,7 @@ function App() {
         </a>
         !
       </h2>
+      <h2>Hello, how are you doing?</h2>
       <p>
         <a
           href="https://github.com/zeit/now-examples/tree/master/create-react-app-functions"
